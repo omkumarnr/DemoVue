d0feeab7f1de92c109ff50825db1e820e9e67b73
omkumarnr <168269819+omkumarnr@users.noreply.github.com>
2024-07-31 14:33:08 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+   components : {GitHub}
+  <h1>Vue Basic Project Skeleton</h1>
+  <GitHub></GitHub>
+</template>
+
+<script>
+import GitHub from './components/views/GitHub.vue';
+export default {
+  name: 'App',
+  components: {
+    
+    GitHub,
+    
+  }
+}
+
+</script>
+
+<style>
+
+</style>
